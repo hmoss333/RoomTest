1aab813c82902f0dd1dcb85c1081a6314cb4a59e
hmoss333 <hdamoss@gmail.com>
2018-05-23 14:05:36 -0400

    Update 5/23/2018
    -Added player sprites and animations (currently single frame, but framework is ready fro full animations)
    -Imported new models for kitchen
    -Configured new models to use interact system
    -Fixed an issue with lighting not working properly (point was not reflecting light properly)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,17 +15,19 @@ public class Player : MonoBehaviour {
     bool updatelastDir = false;
 
     Rigidbody rb;
+    Animator animator;
 
     RaycastHit foundHit;
 
     // Use this for initialization
     void Start () {
         rb = gameObject.GetComponent<Rigidbody>();
-        //itemCount = PlayerPrefs.GetInt("itemCount");
+        animator = gameObject.GetComponentInChildren<Animator>();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        //Control logic
         xInput = Input.GetAxisRaw("Horizontal");
         yInput = Input.GetAxisRaw("Vertical");
 
@@ -46,6 +48,16 @@ public class Player : MonoBehaviour {
             updatelastDir = false;
         }
 
+        //Set direction animations
+        if (lastDir.x > 0)
+            animator.SetTrigger("Right");
+        else if (lastDir.x < 0)
+            animator.SetTrigger("Left");
+        else if (lastDir.z > 0)
+            animator.SetTrigger("Up");
+        else if (lastDir.z < 0)
+            animator.SetTrigger("Down");
+
         //Interact with objects
         if (Input.GetButtonDown("Jump"))
         {
@@ -56,8 +68,9 @@ public class Player : MonoBehaviour {
     void Interact()
     {
         foundHit = new RaycastHit();
-        bool test = Physics.Raycast(transform.position, lastDir, out foundHit, checkDist, 1 << LayerMask.NameToLayer("Interact"));
-        Debug.DrawRay(transform.position, lastDir, Color.green);
+        Vector3 playerBase = new Vector3(transform.position.x, transform.position.y/2, transform.position.z);
+        bool test = Physics.Raycast(playerBase, lastDir, out foundHit, checkDist, 1 << LayerMask.NameToLayer("Interact"));
+        Debug.DrawRay(playerBase, lastDir, Color.green);
 
         if (test)
         {
