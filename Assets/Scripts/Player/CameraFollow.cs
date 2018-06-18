cb011570d472daf1106f5630161a1b09c4a381f8
hmoss333 <hdamoss@gmail.com>
2018-06-18 13:59:40 -0400

    Update 6/18/2018 pt.3
    -Some adjustments to the camera (going forward, should adjust position to look more into the next room when the flashlight is enabled; when flashlight is turned off, function normally)

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -14,8 +14,6 @@ public class CameraFollow : MonoBehaviour {
     [Range(0.01f, 0.1f)]
     public float turnSpeed;
     public float viewDist;
-    //public LayerMask layersToFade;
-    //public LayerMask outerWallLayers;
 
     WaypointManager wpm;
 
@@ -38,54 +36,11 @@ public class CameraFollow : MonoBehaviour {
 
         else
         {
-            ////Set all objects to be visible
-            //if (hits != null)
-            //    ShowAll(hits);
-            //if (wallhits != null)
-            //    ShowAll(wallhits);
-
-            //hits = Physics.RaycastAll(this.transform.position, (player.transform.position - this.transform.position), Vector3.Distance(this.transform.position, player.transform.position), layersToFade);
-            //wallhits = Physics.RaycastAll(this.transform.position, (player.transform.position - this.transform.position), Vector3.Distance(this.transform.position, player.transform.position), outerWallLayers);
-
-            ////Hide objects between the player and the camera
-            //if (hits != null)
-            //    HideHits(hits, 0.5f);
-            //if (wallhits != null)
-            //    HideHits(wallhits, 0.0f);
-
             Vector3 velocity = Vector3.zero;
-            Vector3 forward = player.transform.forward * WaypointManager.scale;// 5f;
+            Vector3 forward = player.transform.forward * WaypointManager.scale;
             Vector3 needPos = player.transform.position - forward * viewDist;
             transform.position = Vector3.SmoothDamp(transform.position, new Vector3(needPos.x, player.transform.position.y + WaypointManager.scale * viewAngle + wpm.levels * WaypointManager.scale, needPos.z), ref velocity, turnSpeed);
             transform.LookAt(player.transform);
-        }
-    }
-
-    void ShowAll(RaycastHit[] hits)
-    {
-        foreach (RaycastHit hit in hits)
-        {
-            Renderer r = hit.collider.GetComponent<Renderer>();
-            if (r)
-            {
-                col = r.material.color;
-                col.a = 1.0f;
-                r.material.color = col;
-            }
-        }
-    }
-
-    void HideHits(RaycastHit[] hits, float alpha)
-    {
-        foreach (RaycastHit hit in hits)
-        {
-            Renderer r = hit.collider.GetComponent<Renderer>();
-            if (r)
-            {
-                col = r.material.color;
-                col.a = alpha;
-                r.material.color = col;
-            }
         }
     }
 }
